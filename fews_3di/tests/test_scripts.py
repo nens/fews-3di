d612c128079bc2f03c8b5b68b48f7d6375077cc8
Reinout van Rees <reinout@vanrees.org>
2020-04-03 16:04:23 +0200

    Made script test more robust (it reacted to pytest cmdln args)

--- fews_3di/tests/test_scripts.py
+++ fews_3di/tests/test_scripts.py
@@ -1,9 +1,12 @@
 """Tests for script.py"""
 from fews_3di import scripts
 
+import mock
+
 
 def test_get_parser():
     parser = scripts.get_parser()
     # As a test, we just check one option. That's enough.
-    options = parser.parse_args()
-    assert not options.verbose
+    with mock.patch("sys.argv", ["program"]):
+        options = parser.parse_args()
+        assert not options.verbose
