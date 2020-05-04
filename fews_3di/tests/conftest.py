5a11a71a803ee9daa8ca2173df9f102a8cca9823
Reinout van Rees <reinout@vanrees.org>
2020-05-04 13:20:52 +0200

    Cleaned up the 'original' directory

--- fews_3di/tests/conftest.py
+++ fews_3di/tests/conftest.py
@@ -20,8 +20,10 @@ EXAMPLE_SETTINGS_FILENAME = "example_settings.xml"
 @pytest.fixture
 def example_settings(tmp_path):
     input_dir = tmp_path / "input"
+    model_dir = tmp_path / "model"
     output_dir = tmp_path / "output"
     input_dir.mkdir()
+    model_dir.mkdir()
     output_dir.mkdir()
 
     shutil.copy(
@@ -30,6 +32,8 @@ def example_settings(tmp_path):
     shutil.copy(TEST_DIR / "example_lateral.csv", input_dir / "lateral.csv")
     shutil.copy(TEST_DIR / "precipitation.nc", input_dir / "precipitation.nc")
     shutil.copy(TEST_DIR / "evaporation.nc", input_dir / "evaporation.nc")
+    shutil.copy(TEST_DIR / "ow.nc", input_dir / "ow.nc")
+    shutil.copy(TEST_DIR / "gridadmin.h5", model_dir / "gridadmin.h5")
 
     settings = utils.Settings(tmp_path / EXAMPLE_SETTINGS_FILENAME)
     return settings
